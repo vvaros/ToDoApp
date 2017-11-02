2eb14889770b8e658a5a4ff11584d61df0f05439
vvaros <vvaros27@gmail.com>
2017-11-03 02:37:23 +0400

    Notification with sound

--- app/src/main/java/com/varos/todolist/receivers/NotificationReceiver.java
+++ app/src/main/java/com/varos/todolist/receivers/NotificationReceiver.java
@@ -5,6 +5,8 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 
 import com.varos.todolist.R;
@@ -15,14 +17,16 @@ public class NotificationReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
 
         try {
+            Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
             NotificationCompat.Builder mBuilder =
                     new NotificationCompat.Builder(context)
                             .setSmallIcon(R.drawable.icon_notification)
                             .setContentTitle(intent.getStringExtra("title"))
-                            .setContentText(intent.getStringExtra("description"));
+                            .setContentText(intent.getStringExtra("description"))
+                            .setSound(alarmSound);
             Intent resultIntent = new Intent(context, MainActivity.class);
             PendingIntent resultPendingIntent =
-                    PendingIntent.getActivity(context, intent.getIntExtra("id",0) , resultIntent,
+                    PendingIntent.getActivity(context, intent.getIntExtra("id", 0), resultIntent,
                             PendingIntent.FLAG_UPDATE_CURRENT);
             mBuilder.setContentIntent(resultPendingIntent);
             NotificationManager mNotificationManager =
